325aa5a6cf13f789bc4b2856d4751de0421ebebe
Alex <alexander.stanley@my.jcu.edu.au>
2018-03-21 21:05:26 +1000

    Prac 4 continued

--- Prac 3/gophers_a_study_of_life_and_death.py
+++ Prac 3/gophers_a_study_of_life_and_death.py
@@ -7,12 +7,11 @@ def main():
 Starting Population: 1000""")
 current_year = 0
 while current_year >= 10:
-        try:
-            birth_rate = birth_rate_gen(birth_rate)
-            death_rate = death_rate_gen(death_rate)
-            population = pop_gen(population)
-        except: break
-        print("Year ", current_year + 1,
+    birth_rate = float(birth_rate_gen(birth_rate))
+    death_rate = float death_rate_gen(death_rate)
+    population = float pop_gen(population)
+
+print("Year ", current_year + 1,
             "*****",
             birth_rate, " gophers were born.", death_rate, " passed, for is the cruel mistress that is nature",
             "Population: ", population)
@@ -23,12 +22,12 @@ else:
 
 
 def birth_rate_gen(birth_rate, population):
-    birth_rate: population * random.int[0.1, 0.2]
+    birth_rate = population * random.int[0.1, 0.2]
     return birth_rate
 
 
 def death_rate_gen(death_rate, population):
-    death_rate: population * random.int[0.05, 0.25]
+    death_rate = population * random.int[0.05, 0.25]
     return death_rate
 
 
